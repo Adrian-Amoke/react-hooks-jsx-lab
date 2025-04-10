f91b11bd6bb2a74cda5c54daa44b667bd7cc1ada
Adrian-Amoke <adrian.amoke@student.moringaschool.com>
2025-04-10 09:36:15 +0300

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+    <nav>
+      <a href="#home" aria-label="Go to Home page">Home</a>
+      <a href="#about" aria-label="Go to About page">About</a>
+    </nav>
+  );
 }
 
 export default NavBar;
